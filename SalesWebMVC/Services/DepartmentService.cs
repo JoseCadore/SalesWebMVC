ae516d527b0a9ee20c649921b355fb675ed4dc09
JCADORE <jose.cadore@prd.cloud>
2020-05-31 16:41:11 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -3,27 +3,25 @@ using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace SalesWebMVC.Services
 {
-    public class SellerService
+    public class DepartmentService
     {
         private readonly SalesWebMVCContext _context;
 
-        public SellerService(SalesWebMVCContext context)
+        public DepartmentService(SalesWebMVCContext context)
         {
             _context = context;
         }
+        
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
 
-        public List<Seller> FindAll()
-        {
-            return _context.Seller.ToList();
-        }
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
     }
+
 }
